a7b4faaa90d90c8b97d8c2f5d0b624bbd5b86147
Leon Friedmann <leon@friedmann.eu>
2021-12-01 20:25:45 +0100

    Added solution for day 1 exercise 2

--- 2021/src/day_01.rs
+++ 2021/src/day_01.rs
@@ -1,31 +1,70 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-pub fn run() -> std::io::Result<()> {
-    let mut file = File::open("./src/files/day01_0_input")?;
+fn get_file_str() -> String {
+    let mut file = File::open("./src/files/day01_0_input").expect("POOF");
     let mut content = String::new();
-    file.read_to_string(&mut content)?;
+    file.read_to_string(&mut content).expect("POOF");
+    return content
+}
+
+
+pub fn run_01() {
+    let content= get_file_str();
 
     let parts = content.split("\n");
     let mut previous_measurement: i32 = i32::MAX;
     let mut increased_counter = 0;
 
     for part in parts {
-        let mut changed_value = String::new();
+        // let mut changed_value = String::new();
         let measurement = part.parse::<i32>().unwrap_or(i32::MIN);
 
         if previous_measurement < measurement {
             increased_counter += 1;
-            changed_value = String::from("Increased");
+            // changed_value = String::from("Increased");
         } else {
-            changed_value = String::from("Decreased");
+            // changed_value = String::from("Decreased");
         }
 
-        println!("{} ({})", part, changed_value);
+        // println!("{} ({})", part, changed_value);
         previous_measurement = measurement;
     }
 
     println!("Total increases: {}", increased_counter);
+}
 
-    Ok(())
+pub fn run_02() {
+
+    let mut increased_counter = 0;
+
+    let content = get_file_str();
+
+    let lines = content
+        .split("\n")
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect::<Vec<_>>();
+
+    /*
+    0  A   ==> a
+    1  A B ==> ab_1
+    2  A B ==> ab_2
+    3    B ==> b
+
+     */
+
+    for index in 0..lines.len() - 3 {
+        let (a, ab_1, ab_2, b) = (lines[index], lines[index + 1], lines[index + 2], lines[index + 3]);
+
+        let left = a + ab_1 + ab_2;
+        let right = ab_1 + ab_2 + b;
+
+        increased_counter += (left < right) as i32;
+        // println!("Index: {}", index);
+        // println!("{} + {} + {} = {}", a, ab_1, ab_2, left);
+        // println!("{} + {} + {} = {}", ab_1, ab_2, b, right);
+        // println!("{}", (left < right) as i32)
+    }
+
+    println!("Total increases: {}", increased_counter)
 }
--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -1,5 +1,12 @@
 mod day_01;
 
 fn main() {
-    day_01::run();
+    let start_1 = std::time::Instant::now();
+    day_01::run_01();
+    let end_1 = std::time::Instant::now();
+    let start_2 = std::time::Instant::now();
+    day_01::run_02();
+    let end_2 = std::time::Instant::now();
+    println!("Task 1: {:?}", end_1 - start_1);
+    println!("Task 2: {:?}", end_2 - start_2);
 }
