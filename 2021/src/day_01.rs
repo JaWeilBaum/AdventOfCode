17cdfb569f029730e6b9295c0a2c2d3930f39443
Leon Friedmann <leon@friedmann.eu>
2021-12-01 09:27:30 +0100

    Day 01 challange 1 completed

--- 2021/src/day_01.rs
+++ 2021/src/day_01.rs
@@ -1,6 +1,31 @@
-pub fn run() {
-    // Statements here are executed when the compiled binary is called
+use std::fs::File;
+use std::io::prelude::*;
 
-    // Print text to the console
-    println!("Hello World!");
+pub fn run() -> std::io::Result<()> {
+    let mut file = File::open("./src/files/day01_0_input")?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+
+    let parts = content.split("\n");
+    let mut previous_measurement: i32 = i32::MAX;
+    let mut increased_counter = 0;
+
+    for part in parts {
+        let mut changed_value = String::new();
+        let measurement = part.parse::<i32>().unwrap_or(i32::MIN);
+
+        if previous_measurement < measurement {
+            increased_counter += 1;
+            changed_value = String::from("Increased");
+        } else {
+            changed_value = String::from("Decreased");
+        }
+
+        println!("{} ({})", part, changed_value);
+        previous_measurement = measurement;
+    }
+
+    println!("Total increases: {}", increased_counter);
+
+    Ok(())
 }
